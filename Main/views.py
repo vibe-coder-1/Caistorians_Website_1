fb7fc48beaee24ce501b58b4b54f4cbda0e0712d
ConstantSeal592 <reubensmith@outlook.com>
2025-12-13 20:00:48 +0000

    Magazine Main menu

--- Main/views.py
+++ Main/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
+from community.models import Story
 
 def homepage(request):
     if request.user.is_authenticated:
         if request.user.school:
             # Redirect or render the user's school homepage
-            return render(request, 'schools/school_home.html', {'school': request.user.school})
+            magazines = Story.objects.filter(school_id=request.user.school.id, is_magazine=True)
+            return render(request, 'schools/school_home.html', {'school': request.user.school, 'magazines': magazines})
         else:
             # User is logged in but has no school assigned
             return render(request, 'Main/homepage.html', {'message': 'You are not assigned to a school yet.'})
