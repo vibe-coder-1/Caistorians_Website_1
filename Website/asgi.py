9f1868dde232f07d9699e672dd193fd6019ff49c
sillybabs <printhavany@gmail.com>
2025-12-13 16:44:32 +0000

    i did some stuff
    i hope chatroom works

--- Website/asgi.py
+++ Website/asgi.py
@@ -16,19 +16,36 @@
 # })
 
 # your_project/asgi.py
+# import os
+# from channels.auth import AuthMiddlewareStack
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from django.core.asgi import get_asgi_application
+# import chat.routing
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Website.settings")
+
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             chat.routing.websocket_urlpatterns
+#         )
+#     ),
+# })
+
+
 import os
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Website.settings")
 
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
-    ),
+    "http": django_asgi_app,
+    "websocket": URLRouter(chat.routing.websocket_urlpatterns),
 })
+
